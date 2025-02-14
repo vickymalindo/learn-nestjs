513273ccc8198ff18d6968180d9de2e76eb197ec
vickymalindo <vicky.malindo@gmail.com>
2025-02-14 13:27:40 +0700

    feat: add car controller

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { CarDto } from './car.dto';
+import { CarService } from './car.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('car')
-export class CarController {}
+export class CarController {
+  constructor(private carService: CarService) {}
+
+  @Get()
+  public getCars() {
+    return this.carService.getCars();
+  }
+
+  @Post()
+  public postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
+  }
+
+  @Get(':id')
+  public getCarById(@Param('id') id: number) {
+    return this.carService.getCarById(id);
+  }
+
+  @Delete(':id')
+  public deleteCarById(@Param('id') id: number) {
+    return this.carService.deleteCarById(id);
+  }
+
+  @Put(':id')
+  public putCarById(@Param('id') id: number, @Query() query) {
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+    return this.carService.putCarById(id, propertyName, propertyValue);
+  }
+}
